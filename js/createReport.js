cd2a74d908498c264100f63473f736ac020b6efa
ajlino <38694859+ajlino@users.noreply.github.com>
2020-07-18 09:50:12 -0400

    Changed to multi-page site Ajax for reports complete

--- js/createReport.js
+++ js/createReport.js
@@ -4,16 +4,30 @@ $(document).ready(function(){
     var post_url=$(this).attr("action");  //get from action url
     var request_method = $(this).attr("method"); //get form GET/POST method
     var form_data = $(this).serialize();  //encode form elements for submission
-    console.log(form_data); //for debugging
+    // console.log(form_data); //for debugging
 
     $.ajax({
       url : post_url,
       type: request_method,
       data : form_data
     }).done(function(response){ //
-  		$("#server-results").html(response);  //innerHTML for div id server-results
+  		// $("#server-results").html(response);  //innerHTML for div id server-results
       var jsonData = JSON.parse(response);
-      console.log(jsonData);
+      // console.log(jsonData);  //for debugging
+
+
+    //Add jsonData to the table
+      var html="";
+      var x=0;
+      html+="<tr>";
+      jsonData.forEach(myFunction);
+      function myFunction(item, index){
+        html+="<td>" + item + "</td>";
+        x++;
+      }
+      html+="</tr>";
+      console.log(x);
+      $("#data").append(html);
   	});
   });
 });
